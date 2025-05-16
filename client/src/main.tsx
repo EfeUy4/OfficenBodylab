d17d8a25eeb0cc6d8e8ae3252335e1a73ac4b6eb
EfeUy4 <efeuwaifo@gmail.com>
2025-05-16 10:11:51 -0400

    added some stuff

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,12 +5,16 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import {BrowserRouter} from 'react-router-dom'
 
 createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
-      <App />
+      <BrowserRouter>
+      <App/>
+      </BrowserRouter>
+X
     </TooltipProvider>
   </QueryClientProvider>
 );
